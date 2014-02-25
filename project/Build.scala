aeed1a8a0efab1d03ec8c2a6c856b6a639f8349a
Hakan <Hako@Hakan-MacBook-Pro.local>
2014-02-25 20:58:05 +0200

    kankaaa

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "Haksle"
+  val appName         = "Kapsle"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
@@ -14,11 +14,18 @@ object ApplicationBuild extends Build {
     "org.jsoup" % "jsoup" % "1.7.2",
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "com.feth"      %%  "play-authenticate" % "0.2.0-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+     resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
+     resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns),
+
+
+     resolvers += Resolver.url("play-authenticate (release)", url("http://joscha.github.com/play-authenticate/repo/releases/"))(Resolver.ivyStylePatterns),
+     resolvers += Resolver.url("play-authenticate (snapshot)", url("http://joscha.github.com/play-authenticate/repo/snapshots/"))(Resolver.ivyStylePatterns)
+     
   )
 
 }
